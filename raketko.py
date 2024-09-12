04c49597a88c94613fa7ce472860491c8c6aef60
TheSunne <114726166+TheSunne@users.noreply.github.com>
2024-09-12 22:03:10 +0600

    Add files via upload

---
+++ raketko.py
@@ -0,0 +1,337 @@
+from .. import loader
+
+@loader.tds
+class plotnaya(loader.Module):
+    """Модуль от @Ceynpeqdxfqdsceyncdpbvkfqghktglf"""
+
+    strings = {"name": "плотная"}
+
+    async def ракеткоcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/7",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def нашазадачаcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/9",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def мистикаcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/37",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def штукаcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/39",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def шаурмаcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/34",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def роналдуcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/41",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def незаметноcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/43",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def мамаcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/45",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def слепойcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/47",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def подзатыльникcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/49",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def извинитеcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/51",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def подстригсяcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/53",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def всебоялисьcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/55",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def споримcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/57",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def фонтанcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/71",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def максcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/73",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def деньcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/76",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def работатьcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/78",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def турникcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/80",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def россияcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/82",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def линейкаcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/84",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def клацаешьcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/101",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def арбузcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/103",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def камриcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/113",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def эплпейcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/115",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def ограблениеcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/117",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def мистерфишcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/106",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def обедпорасписаниюcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/119",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def приколcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/134",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def корпусомcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/136",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def писятьcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/138",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def цзыньcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/140",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def дайдесятьрублейcmd(self, message):
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/verbludraketkopashalko7000/142",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
